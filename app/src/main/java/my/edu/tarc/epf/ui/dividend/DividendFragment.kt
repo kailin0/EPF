9781c6c83d93b5f65a055a119259e365e2bb6414
kailin0 <chankl-wm20@student.tarc.edu.my>
2023-03-17 21:44:51 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/epf/ui/dividend/DividendFragment.kt
@@ -0,0 +1,19 @@
+package my.edu.tarc.epf.ui.dividend
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import my.edu.tarc.epf.R
+
+class DividendFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dividend, container, false)
+    }
+}
